a7b60c230d58e2ee33f56cfafeb9533e4903364a
Markus Engel <markus@markus-VirtualBox.(none)>
2011-12-04 22:52:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edubay::Application.config.secret_token = '29fde714755ee878124619e64f0e83a5917b78af2e5ed5ef8c84a235e08c53dfc15ae831bc87119b833d5d2be4f313252ff95a7b52987bd06770eece63b2f983'
